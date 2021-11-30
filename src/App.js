8fa474e37c92066f4cbeb29ead765637f7616bc8
Ben Ndora <benndora2@gmail.com>
2021-11-30 14:03:41 +0300

    Users-Page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
-import Home from "./pages/home/Home"
-import "./app.css"
+import Home from "./pages/home/Home";
+import UserList from "./pages/userList/UserList";
+import "./app.css";
 
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom"
+ 
 
 function App() {
   return (
-    <div className="App">
+    <Router className="App">
     <Topbar/>
     <div className="container">
     <Sidebar/>
-    <div className="others">
-    <Home/>
+    <Routes>
+    <Route exact path="/"element={<Home/>} />
+    <Route path="/users" element={<UserList/>}/>
+      </Routes>  
     </div>
-    </div>
-    </div>
+    </Router>
   );
 }
 
