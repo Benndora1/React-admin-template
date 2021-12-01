0083a32d77a86f77122aa7f49d612df809b6624d
Ben Ndora <benndora2@gmail.com>
2021-12-01 09:57:05 +0300

    users-table

--- src/pages/userList/UserList.jsx
+++ src/pages/userList/UserList.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./userList.css"
 import { DataGrid } from '@material-ui/data-grid';
+import { DeleteOutline } from '@material-ui/icons'
 
 
 
@@ -11,43 +12,117 @@ function UserList() {
     const columns = [
         { field: 'id', headerName: 'ID', width: 90 },
         {
-          field: 'firstName',
-          headerName: 'First name',
-          width: 150,
+          field: 'user',
+          headerName: 'User',
+          width: 200,
+          renderCell: (params) => {
+              return (
+                  <div className="userListUser">
+                      <img className="userListImg" src={params.row.avatar} alt=""/>
+                      {params.row.username}
+                  </div>
+              )
+          }
+        },
+        {
+          field: 'email',
+          headerName: 'Email',
+          width: 200,
           editable: true,
         },
         {
-          field: 'lastName',
-          headerName: 'Last name',
-          width: 150,
+          field: 'status',
+          headerName: 'Status',
+          width: 140,
           editable: true,
         },
         {
-          field: 'age',
-          headerName: 'Age',
-          type: 'number',
-          width: 110,
-          editable: true,
-        },
-        {
-          field: 'fullName',
-          headerName: 'Full name',
-          description: 'This column has a value getter and is not sortable.',
-          sortable: false,
-          width: 160,
-        },
+            field: 'transaction',
+            headerName: 'Transaction',
+            width: 150,
+            editable: true,
+          }, 
+           {
+            field: 'action',
+            headerName: 'Action',
+            width: 150, 
+            renderCell:(params)=> {
+                return (
+                    <>
+                        <button className="userListEdit">Edit</button>
+                         <DeleteOutline className="userListDelete"/>
+                    </>
+                )
+            }
+
+
+        }
       ];
       
       const rows = [
-        { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-        { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-        { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-        { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-        { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-        { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-        { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-        { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-        { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+        { id: 1, username: 'Jon Snow',
+         avatar:"https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+         email: "hon@gmail.com",
+         status: 'active',
+         transaction: "$120"
+          },
+          { id: 2, username: 'Jon Snow',
+          avatar:"https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+          email: "hon@gmail.com",
+          status: 'active',
+          transaction: "$120"
+           },
+           { id: 3, username: 'Jon Snow',
+           avatar:"https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+           email: "hon@gmail.com",
+           status: 'active',
+           transaction: "$120"
+            },
+            { id: 4, username: 'Jon Snow',
+            avatar:"https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+            email: "hon@gmail.com",
+            status: 'active',
+            transaction: "$120"
+             },
+             { id: 5, username: 'Jon Snow',
+             avatar:"https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+             email: "hon@gmail.com",
+             status: 'active',
+             transaction: "$120"
+              },
+              { id: 6, username: 'Jon Snow',
+              avatar:"https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+              email: "hon@gmail.com",
+              status: 'active',
+              transaction: "$120"
+               },
+              { id: 7, username: 'Jon Snow',
+               avatar:"https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+               email: "hon@gmail.com",
+               status: 'active',
+               transaction: "$120"
+                },
+                { id: 8, username: 'Jon Snow',
+                avatar:"https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+                email: "hon@gmail.com",
+                status: 'active',
+                transaction: "$120"
+                 },
+                 { id: 9, username: 'Jon Snow',
+                 avatar:"https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+                 email: "hon@gmail.com",
+                 status: 'active',
+                 transaction: "$120"
+                  },
+                  { id: 10, username: 'Jon Snow',
+                  avatar:"https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+                  email: "hon@gmail.com",
+                  status: 'active',
+                  transaction: "$120"
+                   }
+
+             
+    
       ];
     return (
         <div className="userList">
@@ -55,7 +130,7 @@ function UserList() {
               <DataGrid
         rows={rows}
         columns={columns}
-        pageSize={5}
+        pageSize={10}
         checkboxSelection
         disableSelectionOnClick
       />
